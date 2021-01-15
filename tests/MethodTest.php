bbbc9c03b6d72415b770928edc0205519c3093d8
Michael McLarnon <michaelmclarnon@hotmail.co.uk>
2021-01-15 16:38:44 +0000

    feat(Punk API): related to #1 Building an API for craft beer
    Nuked 20 lines by refactoring MethodTest.php using setUp() instead of repeating the same object setup code for each test.

--- tests/MethodTest.php
+++ tests/MethodTest.php
@@ -10,17 +10,25 @@ use Zleet\PunkAPI\Method;
  */
 class MethodTest extends \PHPUnit\Framework\TestCase
 {
+    // private property to hold the Method object that is constructed by
+    // _ before testing
+    private $methodObject;
+
+    // set up a Method object for use in testing
+    protected function setUp(): void
+    {
+        $temperature = new Temperature('45', 'celcius');
+        $mashTemperature = new MashTemperature($temperature, 99);
+        $fermentation = new Fermentation($temperature);
+        $this->methodObject = new Method($mashTemperature, $fermentation, 'A tasty lemon.');
+    }
+
     /**
      * Test creating a Method class
      */
     public function testClassCreation()
     {
-        $temperature = new Temperature('45', 'celcius');
-        $mashTemperature = new MashTemperature($temperature, 99);
-        $fermentation = new Fermentation($temperature);
-        $method = new Method($mashTemperature, $fermentation, 'A tasty lemon.');
-
-        $this->assertInstanceOf(Method::class, $method);
+        $this->assertInstanceOf(Method::class, $this->methodObject);
     }
 
     /**
@@ -29,16 +37,9 @@ class MethodTest extends \PHPUnit\Framework\TestCase
      */
     public function testGetMashTemperature()
     {
-        $temperature = new Temperature('45', 'celcius');
-        $mashTemperature = new MashTemperature($temperature, 99);
-        $fermentation = new Fermentation($temperature);
-        $method = new Method($mashTemperature, $fermentation, 'A tasty lemon.');
-
         $this->assertInstanceOf(MashTemperature::class,
-            $method->getMashTemperature());
+            $this->methodObject->getMashTemperature());
     }
-
-    // test getting Fermentation
 
     /**
      * Test creating a Method object then retrieving the Fermentation
@@ -46,13 +47,8 @@ class MethodTest extends \PHPUnit\Framework\TestCase
      */
     public function testGettingFermentation()
     {
-        $temperature = new Temperature('45', 'celcius');
-        $mashTemperature = new MashTemperature($temperature, 99);
-        $fermentation = new Fermentation($temperature);
-        $method = new Method($mashTemperature, $fermentation, 'A tasty lemon.');
-
         $this->assertInstanceOf(Fermentation::class,
-            $method->getFermentation());
+            $this->methodObject->getFermentation());
 
     }
 
@@ -61,27 +57,15 @@ class MethodTest extends \PHPUnit\Framework\TestCase
      */
     public function testGettingTwist()
     {
-        $temperature = new Temperature('45', 'celcius');
-        $mashTemperature = new MashTemperature($temperature, 99);
-        $fermentation = new Fermentation($temperature);
-        $method = new Method($mashTemperature, $fermentation, 'A tasty lemon.');
-
-        $this->assertEquals('A tasty lemon.', $method->getTwist());
+        $this->assertEquals('A tasty lemon.', $this->methodObject->getTwist());
     }
-
-    // test getting the Method object as an array
 
     /**
      * Test generating an array representation of a Method object
      */
     public function testGettingMethodAsAnArray()
     {
-        $temperature = new Temperature('45', 'celcius');
-        $mashTemperature = new MashTemperature($temperature, 99);
-        $fermentation = new Fermentation($temperature);
-        $method = new Method($mashTemperature, $fermentation, 'A tasty lemon.');
-
-        $this->assertIsArray($method->toArray(), "method->toArray() does not return an array");
+        $this->assertIsArray($this->methodObject->toArray(), "method->toArray() does not return an array");
     }
 
 }
