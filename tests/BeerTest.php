a5d5b4609bb3a0c8b755b4ecbaf0d9b3dda729f0
Michael McLarnon <michaelmclarnon@hotmail.co.uk>
2021-01-21 15:43:01 +0000

    feat(Punk API): related to #1 Building an API for craft beer
    Deleted old 'build a Beer object for testing code' from BeetTest.php and replaced it with a call to Beer::fromArray()

--- tests/BeerTest.php
+++ tests/BeerTest.php
@@ -23,138 +23,15 @@ class BeerTest extends \PHPUnit\Framework\TestCase
     // set up a Beer object for use in testing
     protected function setUp(): void
     {
+        // read the information for a new Beer object from a local JSON file
+        $beerJson = file_get_contents("tests/single_beer_json.json");
+        $beerInfo = json_decode($beerJson, 1);
+
         // build the beer object
-        $beer = $this->createBeerObjectForTesting();
+        $beer = Beer::fromArray($beerInfo);
 
         // store the beer object for testing
         $this->beerObject = $beer;
-    }
-
-    /**
-     * Helper function for the setUp() method. Builds a Beer object from a JSON
-     * file which contains all the information for a Beer object.
-     */
-    private function createBeerObjectForTesting()
-    {
-        // read all of the information required for setting up a Beer object for
-        // testing from a local json file
-        $beerJson = file_get_contents('tests/single_beer_json.json');
-        $beerInfo = json_decode($beerJson, 1);
-
-        // =================================================================
-        // set up all the value objects that are used to build a Beer Object
-        // =================================================================
-        // build a Volume value object
-        $volume = new Volume(
-            $beerInfo["volume"]["value"],
-            $beerInfo["volume"]["unit"]
-        );
-
-        // build a BoilVolume value object
-        $boilVolume = new BoilVolume(
-            $beerInfo["boil_volume"]["value"],
-            $beerInfo["boil_volume"]["unit"]
-        );
-
-        // build a Temperature value object to place inside the MashTemperature
-        // value object
-        $temperature1 = new Temperature(
-             $beerInfo["method"]["mash_temp"][0]["temp"]["value"],
-             $beerInfo["method"]["mash_temp"][0]["temp"]["unit"]
-        );
-        // build a MashTemperature value object
-        $mashTemperature = new MashTemperature(
-            $temperature1,
-            $beerInfo["method"]["mash_temp"][0]["duration"]
-        );
-        // create an array to hold the single MashTemperature object
-        $mashTemperatures = [$mashTemperature];
-        // build a Temperature value object to place inside the Fermentation
-        // value object
-        $temperature2 = new Temperature(
-            $beerInfo["method"]["fermentation"]["temp"]["value"],
-            $beerInfo["method"]["fermentation"]["temp"]["unit"]
-        );
-        // build a Fermentation value object
-        $fermentation = new Fermentation($temperature2);
-        // build a Method value object (use the array of one Mash Temperature
-        // object and the Fermentation object, built above, along with the
-        // twist string value from $beerInfo to create it)
-        $method = new Method(
-            $mashTemperatures,
-            $fermentation,
-            $beerInfo["method"]["twist"]
-        );
-        // build an array of Malt objects
-        $maltArrays = $beerInfo["ingredients"]["malt"];
-        $arrayOfMaltObjects = [];
-        foreach ($maltArrays as $maltArray) {
-            // build new Amount object
-            $amountObject = new Amount(
-                $maltArray["amount"]["value"],  // value
-                $maltArray["amount"]["unit"]    // unit
-            );
-            // build new Malt object
-            $maltObject = new Malt(
-                $maltArray["name"], // name
-                $amountObject       // amount
-            );
-            // store the Malt object
-            $arrayOfMaltObjects[] = $maltObject;
-        }
-        // build an array of Hop objects
-        $hopArrays = $beerInfo["ingredients"]["hops"];
-        $arrayOfHopObjects = [];
-        foreach ($hopArrays as $hopArray) {
-            // build new amount object
-            $amountObject = new Amount(
-                $hopArray["amount"]["value"],  // value
-                $hopArray["amount"]["unit"]    // unit
-            );
-            // build new Hop object
-            $hopObject = new Hop(
-                $hopArray["name"],      // name
-                $amountObject,          // amount
-                $hopArray["add"],       // add
-                $hopArray["attribute"]  // attribute
-            );
-            // store the Hop object
-            $arrayOfHopObjects[] = $hopObject;
-        }
-        // build a Ingredients value object (using the array of Malt objects,
-        // the array of Hop objects and the yeast value from $beerInfo
-        $ingredients = new Ingredients(
-            $arrayOfMaltObjects,
-            $arrayOfHopObjects,
-            $beerInfo["ingredients"]["yeast"]
-        );
-        // build the Beer object (using information from $beerInfo, along with
-        // several of the value objects created above)
-        $beer = new Beer(
-            $beerInfo["id"],                // id
-            $beerInfo["name"],              // name
-            $beerInfo["tagline"],           // tagline
-            $beerInfo["first_brewed"],      // firstBrewed
-            $beerInfo["description"],       // description
-            $beerInfo["image_url"],         // imageUrl
-            $beerInfo["abv"],               // abv
-            $beerInfo["ibu"],               // ibu
-            $beerInfo["target_fg"],         // targetFg
-            $beerInfo["target_og"],         // targetOg
-            $beerInfo["ebc"],               // ebc
-            $beerInfo["srm"],               // srm
-            $beerInfo["ph"],                // ph
-            $beerInfo["attenuation_level"], // attenuationLevel
-            $volume,                        // volume
-            $boilVolume,                    // boilVolume
-            $method,                        // method
-            $ingredients,                   // ingredients
-            $beerInfo["food_pairing"],      // foodPairing
-            $beerInfo["brewers_tips"],      // brewersTips
-            $beerInfo["contributed_by"]     // contributedBy
-        );
-
-        return $beer;
     }
 
     /**
@@ -165,24 +42,31 @@ class BeerTest extends \PHPUnit\Framework\TestCase
         $this->assertInstanceOf(Beer::class, $this->beerObject);
     }
 
-    // ===========================
-    // test all the getter methods
-    // ===========================
-    // test getting id
+    /**
+     * test getting id
+     */
     public function testGettingAnId()
     {
-        $this->assertEquals(1, $this->beerObject->getId(),
-            'Beer->getId() does not return 1');
+        $this->assertEquals(1,
+            $this->beerObject->getId(),
+            'Beer->getId() does not return 1'
+        );
     }
 
-    // test getting name
+    /**
+     * test getting name
+     */
     public function testGettingTheName()
     {
-        $this->assertEquals('Buzz', $this->beerObject->getName(),
-            "Beer->getName() doesn't return 'Buzz'");
+        $this->assertEquals('Buzz',
+            $this->beerObject->getName(),
+            "Beer->getName() doesn't return 'Buzz'"
+        );
     }
 
-    // test getting tagline
+    /**
+     * test getting tagline
+     */
     public function testGettingTheTagline()
     {
         $this->assertEquals(
@@ -193,16 +77,21 @@ class BeerTest extends \PHPUnit\Framework\TestCase
 
     }
 
-    // test getting firstBrewed
+    /**
+     * test getting firstBrewed
+     */
     public function testGettingFirstBrewed()
     {
-        $this->assertEquals("09/2007",
+        $this->assertEquals(
+            "09/2007",
             $this->beerObject->getFirstBrewed(),
             "Beer->getFirstBrewed() doesn't return '09/2007'"
         );
     }
 
-    // test getting description
+    /**
+     * test getting description
+     */
     public function testGettingDescription()
     {
         $this->assertEquals("A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once.",
@@ -211,72 +100,106 @@ class BeerTest extends \PHPUnit\Framework\TestCase
         );
     }
 
-    // test getting imageUrl
+    /**
+     * test getting imageUrl
+     */
     public function testGettingImageUrl()
     {
-        $this->assertEquals("https://images.punkapi.com/v2/keg.png",
+        $this->assertEquals(
+            "https://images.punkapi.com/v2/keg.png",
             $this->beerObject->getImageUrl(),
-            "Beer->getImageUrl() doesn't return https://images.punkapi.com/v2/keg.png"
+            "Beer->getImageUrl() doesn't return "
+            . "https://images.punkapi.com/v2/keg.png"
         );
     }
 
-    // test getting abv
+    /**
+     * Test getting abv
+     */
     public function testGettingAbv()
     {
-        $this->assertEquals(4.5,  $this->beerObject->getAbv(),
+        $this->assertEquals(
+            4.5,
+            $this->beerObject->getAbv(),
             "Beer->getAbv() doesn't return 4.5"
         );
     }
 
-    // test getting ibu
+    /**
+     * Test getting ibu
+     */
     public function testGettingIbu()
     {
-        $this->assertEquals(60,  $this->beerObject->getIbu(),
+        $this->assertEquals(
+            60,
+            $this->beerObject->getIbu(),
             "Beer->getIbu() doesn't return 60"
         );
     }
 
-    // test getting targetFg
+    /**
+     * Test getting targetFg
+     */
     public function testGettingTargetFg()
     {
-        $this->assertEquals(1010,  $this->beerObject->getTargetFg(),
+        $this->assertEquals(
+            1010,
+            $this->beerObject->getTargetFg(),
             "Beer->getTargetFg() doesn't return 1010"
         );
     }
 
-    // test getting targetOg
+    /**
+     * Test getting target Og
+     */
     public function testGettingTargetOg()
     {
-        $this->assertEquals(1044,  $this->beerObject->getTargetOg(),
+        $this->assertEquals(
+            1044,
+            $this->beerObject->getTargetOg(),
             "Beer->getTargetOg() doesn't return 1044"
         );
     }
 
-    // test getting ebc
+    /**
+     * Test getting ebc
+     */
     public function testGettingEbc()
     {
-        $this->assertEquals(20,  $this->beerObject->getEbc(),
+        $this->assertEquals(
+            20,
+            $this->beerObject->getEbc(),
             "Beer->getEbc() doesn't return 20"
         );
     }
 
-    // test getting srm
+    /**
+     * Test getting srm
+     */
     public function testGettingSrm()
     {
-        $this->assertEquals(10,  $this->beerObject->getSrm(),
+        $this->assertEquals(
+            10,
+            $this->beerObject->getSrm(),
             "Beer->getSrm() doesn't return 10"
         );
     }
 
-    // test getting ph
+    /**
+     * Test getting ph
+     */
     public function testGettingPh()
     {
-        $this->assertEquals(4.4,  $this->beerObject->getPh(),
+        $this->assertEquals(
+            4.4,
+            $this->beerObject->getPh(),
             "Beer->getPh() doesn't return 4.4"
         );
     }
 
-    // test getting attenuationLevel
+    /**
+     * Test getting attenuationLevel
+     */
     public function testGettingAttenuationLevel()
     {
         $this->assertEquals(
@@ -377,10 +300,6 @@ class BeerTest extends \PHPUnit\Framework\TestCase
         // read Beer information from local file
         $singleBeerInfo = file_get_contents('tests/single_beer_json.json');
         $singleBeerInfo = json_decode($singleBeerInfo, 1);
-
-        // test print
-        echo "\nsingleBeerInfo:\n";
-        print_r($singleBeerInfo);
 
         $beer = Beer::fromArray($singleBeerInfo);
 
